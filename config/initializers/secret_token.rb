ee15ef619a168ec1d2de2965046fdc75a7340cf3
Rustam Musabaev <mr-2005@mail.ru>
2013-05-10 19:01:59 +0500

    1

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Lgshop::Application.config.secret_token = '891e549f310116334c3a7905871dee31cd4b3a6db8436f39d97cd5fe1318be9890693c5c405e7838b5bdd5cf7be80f99796f7e4158fd0d34d9db830b347124cd'
