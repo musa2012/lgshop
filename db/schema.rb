37336d466731d7cb3160a83a6b97bbed0c33fedb
Rustam Musabaev <mr-2005@mail.ru>
2013-05-12 08:01:40 +0500

    hasbrains

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130512013126) do
+ActiveRecord::Schema.define(:version => 20130512025220) do
+
+  create_table "microposts", :force => true do |t|
+    t.string   "contant"
+    t.integer  "user_id"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
 
   create_table "products", :force => true do |t|
     t.string   "name"
